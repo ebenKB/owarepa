225e77202f6df9482a9902b863dfb84c6325bdfb
ebenKB <eakbo23@gmail.com>
2020-10-09 11:44:43 +0000

    bring content to the front

--- components/BenefitsSection.js
+++ components/BenefitsSection.js
@@ -20,7 +20,10 @@ const BenefitsSection = () => {
           </li>
           <li className="pb-8">
             <h3 className="text-3xl">It is a memory booster</h3>
-            <p>One needs to keep count of the number of seeds in each hole on the board as be contemplates his next move. 
+            <p>
+              One needs to keep count of the number of seeds in each hole on the board as he contemplates his next move. 
+            </p>
+            <p>
               As a player simulates in his mind his potential moves and eventually the opponent's counter moves, 
               he needs to keep a mental picture of how many seeds are in each hole.
             </p>
@@ -31,6 +34,8 @@ const BenefitsSection = () => {
               Since your objective is to win more seeds, 
               you do not only think of moves that will make you capture your opponent's seeds, 
               but moves that will eventually limit your opponent's chances of capturing many more seeds. 
+            </p>
+            <p>
               In Abapa, you sometimes have to sacrifice some seeds with the objective of either capturing 
               more seeds afterward, or getting the chance to rearrange the seeds on the board to your favor.
             </p>
