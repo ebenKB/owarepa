80fe5a2400d07ce7a9f24643de15657d6f5b0596
ebenKB <eakbo23@gmail.com>
2020-12-01 10:37:52 +0000

    add nav link to header

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react';
 import HeaderStyles from '../styles/Header.module.css';
 import { AppContext } from '../context/app.context';
+import Link from 'next/Link'
 
 const Header = ({ handleAction }) => {
   const [yPos, setYPos] = useState(0);
@@ -38,10 +39,12 @@ const Header = ({ handleAction }) => {
   return (
     <div className={`flex ${HeaderStyles.app_header} ${canShowHeader() ? HeaderStyles.app_header__opaque : HeaderStyles.app_header__transparent}`}>
       <div className="flex-1 text-2xl">
-        <span className="inline-flex items-center m-auto">
-          <img src="/logo.png" alt="" className={`${HeaderStyles.app_logo } w-12`} />
-          <span className="ml-2 text-3xl">owarepa</span>
-        </span>
+        <Link href="/">
+          <span className="inline-flex items-center m-auto">
+            <img src="/logo.png" alt="" className={`${HeaderStyles.app_logo } w-12`} />
+            <span className="ml-2 text-3xl">owarepa</span>
+          </span>
+        </Link>
       </div>
       <div className="flex-1 text-right">
           <button onClick={handleHamBurgerClick} className="w-8" >
